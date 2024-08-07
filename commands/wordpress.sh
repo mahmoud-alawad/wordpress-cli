325c22a92950dd0b1f824c8102734ca7e988bc23
mahmoud <awad25.ma@gmail.com>
2024-08-07 14:15:57 +0200

    naming convention

--- commands/wordpress.sh
+++ commands/wordpress.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
 function download_wordpress() {
-    local target_dir=$1
+    local targetDir=$1
 
-    if [ -z "$target_dir" ]; then
+    if [ -z "$targetDir" ]; then
         bash "$PACKGEPATH/../utils/message.sh" warning "Error: No directory specified."
         display_help
         exit 1
     fi
 
-    pushd ${target_dir} || exit
+    pushd ${targetDir} || exit
 
     bash "$PACKGEPATH/../utils/message.sh" info "Downloading WordPress..."
 
-    curl -o "$target_dir/latest.tar.gz" https://wordpress.org/latest.tar.gz
+    curl -o "$targetDir/latest.tar.gz" https://wordpress.org/latest.tar.gz
 
     bash "$PACKGEPATH/../utils/message.sh" info "Extracting WordPress..."
-    tar -xzf "$target_dir/latest.tar.gz" -C "$target_dir"
-    rm -rf "$target_dir/latest.tar.gz"
+    tar -xzf "$targetDir/latest.tar.gz" -C "$targetDir"
+    rm -rf "$targetDir/latest.tar.gz"
 
     read -p "Enter folder name or leave blank to put all files in the directory specified: "$'\n' folder
-    mkdir "$target_dir/$folder"
+    mkdir "$targetDir/$folder"
 
     bash "$PACKGEPATH/../utils/message.sh" info "folder name is $folder"
 
     if [ -d "$PWD/$folder" ]; then
-        mv -f "$target_dir/wordpress/"* "$target_dir/$folder"
+        mv -f "$targetDir/wordpress/"* "$targetDir/$folder"
     else
         bash "$PACKGEPATH/../utils/message.sh" warning "no folder founded fallback to the current directory..."
-        mv -f "$target_dir/wordpress/"* "$PWD"
+        mv -f "$targetDir/wordpress/"* "$PWD"
     fi
 
     dockerizeWordpress $folder
 
-    rm -rf "$target_dir/wordpress/"
+    rm -rf "$targetDir/wordpress/"
 
-    bash "$PACKGEPATH/../utils/message.sh" success "WordPress has been downloaded and extracted to $target_dir/$folder"
+    bash "$PACKGEPATH/../utils/message.sh" success "WordPress has been downloaded and extracted to $targetDir/$folder"
 }
 
 function download_bedrock() {
-    local target_dir=$1
+    local targetDir=$1
 
     if command -v git &>/dev/null; then
         bash "$PACKGEPATH/../utils/message.sh" success "Git is installed. Version: $(git --version)"
@@ -48,7 +48,7 @@ function download_bedrock() {
         exit 1
     fi
 
-    pushd ${target_dir} || exit
+    pushd ${targetDir} || exit
     read -p "Enter folder name or leave blank to put all files in the directory specified: "$'\n' folder
 
     git clone https://github.com/roots/bedrock.git "$folder"
@@ -69,7 +69,7 @@ function download_bedrock() {
 }
 
 function download_sage() {
-    local target_dir=$1
+    local targetDir=$1
 
     if command -v git &>/dev/null; then
         bash "$PACKGEPATH/../utils/message.sh" success "Git is installed. Version: $(git --version)"
@@ -78,7 +78,7 @@ function download_sage() {
         exit 1
     fi
 
-    pushd ${target_dir} || exit
+    pushd ${targetDir} || exit
     read -p "Enter folder name or leave blank to put all files in the directory specified: "$'\n' folder
     if [ -z "$var" ]; then
         git clone https://github.com/roots/sage.git
@@ -95,8 +95,8 @@ function dockerizeWordpress() {
         read -p "Do you wish to Dockerize this app? " yn
         case $yn in
         [Yy]*)
-            cp -r "$DOCKERFILES_DIR/"* "$PWD/$folder/"
-            cp -r "$DOCKERFILES_DIR/".* "$PWD/$folder/"
+            cp -r "$DOCKERFILESDIR/"* "$PWD/$folder/"
+            cp -r "$DOCKERFILESDIR/".* "$PWD/$folder/"
             break
             ;;
         [Nn]*) exit ;;
